953e70bf4360943096e26452cca8baef64c3ae2c
DIR\ramon.silva <ramon.silva@concretesolutions.com.br>
2018-08-13 13:13:05 -0300

    - MongoDB Creating Objects and Filtering

---
+++ mongo-demo/index.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/playground')
+    .then(() =>{
+        console.log('Connected to MongoDB...')
+    })
+    .catch(err => {
+        console.error('Could not connect to mongoDB', err);
+    });
+
+const courseSchema = mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [ String ],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean
+});
+const Course = mongoose.model('Course', courseSchema);
+
+async function createCourse() {
+    const course = new Course({
+        name:'Node.JS Course',
+        author:'Mosh',
+        tags: ['Node.JS', 'Backend'],
+        isPublished: true
+    });
+    const result = await course.save();
+    console.log(result);
+}
+
+async function getCourses() {
+    const courses = await Course
+    .find({ author: 'Mosh' , isPublished: true})
+    .limit(10)
+    .sort({ name: 1})
+    .select({ name: 1, author: 1});
+    console.log(courses);
+}
+
+// createCourse();
+getCourses();
+
